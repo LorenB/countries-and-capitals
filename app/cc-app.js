49ec878763d3c1d8bd760d5bd1d6c5faa686e107
LorenB <loren.barcus@gmail.com>
2014-07-14 21:02:26 -0400

    Refactored app/cc-app.js to us promises for defered execution and added more robust http request handling.

--- app/cc-app.js
+++ app/cc-app.js
@@ -28,11 +28,6 @@ angular.module('ccApp', ['ui.bootstrap', 'ngRoute'])
                 cacRequest: [ '$route', '$http', '$q', function ($route, $http, $q) {
                     var data = $q.defer();
                     var url = "http://api.geonames.org/countryInfoJSON?formatted=true&lang=en&username=lorenb&style=full";
-//                    var request = {
-//                        outputMode: 'json',
-//                        showSourceText: '1',
-//                        call_back: 'JSON_CALLBACK'
-//                    };
                     $http({
                         method: 'GET',
                         url: url
@@ -46,31 +41,43 @@ angular.module('ccApp', ['ui.bootstrap', 'ngRoute'])
             }
         })
     })
-    .controller('CountryCtrl', function ($scope, $http, $routeParams) {
-//        neighboursJSON ?
-        $http.get('http://api.geonames.org/countryInfoJSON?formatted=true&lang=en&username=lorenb&style=full&country=' + $routeParams.countryCode)
-        .success(function (data) {
-            $scope.country = data;
-        });
-        $http.get('http://api.geonames.org/neighboursJSON?formatted=true&lang=en&username=lorenb&country=' + $routeParams.countryCode)
-        .success(function (data) {
-            $scope.countryNeighbors = data;
-        });
-
+    .controller('CountryCtrl', function ($scope, $http, $routeParams, cacCountryRequest, cacNeighboursRequest) {
+        $scope.country = cacCountryRequest;
+        $scope.countryNeighbors = cacNeighboursRequest;
     })
     .config(function ($routeProvider) {
         $routeProvider.when('/country/:countryCode', {
             templateUrl: './country.html',
-            controller: 'CountryCtrl'
+            controller: 'CountryCtrl',
+            resolve: {
+                cacNeighboursRequest: [ '$route', '$http', '$q', function ($route, $http, $q) {
+                    var neighboursData = $q.defer();
+                    var neighboursUrl = 'http://api.geonames.org/neighboursJSON?formatted=true&lang=en&username=lorenb&country=' + $route.current.params.countryCode;
+                    $http({
+                        method: 'GET',
+                        url: neighboursUrl
+                    }).success(function (result) {
+                        neighboursData.resolve(result);
+                    }).error(function () {
+                        alert('error');
+                    });
+                    data = neighboursData.promise;
+                    return data;
+                }],
+                cacCountryRequest: [ '$route', '$http', '$q', function ($route, $http, $q) {
+                    var countryData = $q.defer();
+                    var countryUrl = 'http://api.geonames.org/countryInfoJSON?formatted=true&lang=en&username=lorenb&style=full&country=' + $route.current.params.countryCode;
+                    $http({
+                        method: 'GET',
+                        url: countryUrl
+                    }).success(function (result) {
+                        countryData.resolve(result);
+                    }).error(function () {
+                        alert('error');
+                    });
+                    data = countryData.promise;
+                    return data;
+                }]
+            }
         })
-    })
-    /*
-     .controller('CountriesCtrl', function($scope){
-     //empty
-     })
-     .config(function($routeProvider){
-     $routeProvider.when('/', {
-     templateUrl : './countries.html'
-     controller : 'CountriesCtrl'
-     })
-     })*/;
+    });
